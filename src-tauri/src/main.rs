1cc0fff7075224d5f6fed45f4d394ad29ed0259f
afa <rmingwang@gmail.com>
2022-04-29 23:52:33 +0800

    test http.fetch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,11 +7,17 @@ use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app|{
+      let win = app.get_window("main").unwrap();
+      win.show().unwrap();
+      win.open_devtools();
+      Ok(())
+    })
     .build(tauri::generate_context!())
     .expect("error while running tauri application")
     .run(|app, e| match e {
       tauri::RunEvent::WindowEvent { label, event, .. } => {
-        println!("{:?}", event);
+        // println!("{:?}", event);
         match event {
           tauri::WindowEvent::CloseRequested { api, ..} =>{
             api.prevent_close();
